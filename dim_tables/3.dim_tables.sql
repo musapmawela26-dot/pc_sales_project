57b2c8a8cbe26dbefd256db33cf463b76be78e37
Musa Percy Mawela <musapmawela26@gmail.com>
2026-04-22 18:55:35 +0200

    dim tables

---
+++ dim_tables/3.dim_tables.sql
@@ -0,0 +1,13 @@
+--dimensional tables
+
+select * from [PC_Salesdb].[dbo].[dim_channel]
+select * from [PC_Salesdb].[dbo].[dim_customer]
+select * from [PC_Salesdb].[dbo].[dim_date]
+select * from [PC_Salesdb].[dbo].[dim_employee]
+select * from [PC_Salesdb].[dbo].[dim_location]
+select * from [PC_Salesdb].[dbo].[dim_payment_method]
+select * from [PC_Salesdb].[dbo].[dim_pc]
+select * from [PC_Salesdb].[dbo].[dim_priority]
+select * from [PC_Salesdb].[dbo].[dim_shop]
+select * from [PC_Salesdb].[dbo].[PC_Data]
+select * from [PC_Salesdb].[dbo].[dim_fact_sales]
